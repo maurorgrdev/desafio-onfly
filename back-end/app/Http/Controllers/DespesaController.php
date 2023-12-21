32960ce58bffcb39944178c1ca9e2a0c836f0be0
Mauro Roger <mauroroger@MacBook-Air-de-Mauro.local>
2023-12-21 13:38:54 -0400

    Inicializar API | feat: migration de despesas | feat:model de despesas com flag --all no terminal

---
+++ back-end/app/Http/Controllers/DespesaController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreDespesaRequest;
+use App\Http\Requests\UpdateDespesaRequest;
+use App\Models\Despesa;
+
+class DespesaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreDespesaRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreDespesaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Despesa  $despesa
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Despesa $despesa)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Despesa  $despesa
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Despesa $despesa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateDespesaRequest  $request
+     * @param  \App\Models\Despesa  $despesa
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateDespesaRequest $request, Despesa $despesa)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Despesa  $despesa
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Despesa $despesa)
+    {
+        //
+    }
+}
---
+++ back-end/database/migrations/2023_12_21_172617_create_despesas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDespesasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('despesas', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 191)->nullable();
+            $table->date('data')->nullable();
+            $table->float('valor')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('despesas');
+    }
+}
